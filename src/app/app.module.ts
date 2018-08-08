b40a8da2ce6bd27797ad5f01d5701b2950e84724
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-08-08 11:52:28 -0500

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { GreenWrapperComponent } from './green-wrapper.component';
 import { BlueWrapperComponent, BlueWrapperDirective } from './blue-wrapper.component';
+import { GreenWrapperComponent } from './green-wrapper.component';
 
 @NgModule({
   declarations: [
